977313fee97dbb70d1db5a3f1d57508d91643384
rameshu08 <rameshu507@gmail.com>
2020-08-03 21:05:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import Test from './test'
+// import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text"></input>
+        <button type="button">ADD TODO</button>
       </header>
     </div>
   );
 }
+
+
+// class Welcome extends Component {
+//   render() { 
+//     return <h1>Hello From Ramesh</h1>
+//   }
+// }
+ 
+// export default Welcome;
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function test() {
+    return (
+        <h1>Hello from Test</h1>
+    )
+}
+
+export default test;
